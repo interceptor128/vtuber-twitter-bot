0c8b60a7388026f5fa0b45b59e8bd18689982e7c
interceptor128 <polestar.interceptor.0128@gmail.com>
2020-06-08 11:10:26 +0900

    add member

--- dics.py
+++ dics.py
@@ -1,29 +1,41 @@
 # botが見るVTuberのリスト
-members = ["七瀬すばる", "しぐれうい"]
+members = ["七瀬すばる", "しぐれうい", "カグラナナ", "名取さな", "セプタ"]
 
 # YouTube Ch の Channel ID　の辞書
 YT_ID_dic = {
-    "七瀬すばる": "UCji4kFR4n5TnEpqga0SHxlQ",
-    "しぐれうい": "UCt30jJgChL8qeT9VPadidSw"
+    "七瀬すばる" : "UCji4kFR4n5TnEpqga0SHxlQ",
+    "しぐれうい" : "UCt30jJgChL8qeT9VPadidSw",
+    "カグラナナ" : "UCbfv8uuUXt3RSJGEwxny5Rw",
+    "名取さな"  : "UCIdEIHpS0TdkqRkHL5OkLtA",
+    "セプタ"    : "UC-6lyT--LXvJaoqn7Id4jMQ" 
 }
 
 # Twitter mobile版URLの辞書
 tw_url_dic = {
-    "七瀬すばる": "https://mobile.twitter.com/nanase_subaru_",
-    "しぐれうい": "https://mobile.twitter.com/ui_shig"
+    "七瀬すばる" : "https://mobile.twitter.com/nanase_subaru_",
+    "しぐれうい" : "https://mobile.twitter.com/ui_shig",
+    "カグラナナ" : "https://mobile.twitter.com/nana_kaguraaa",
+    "名取さな"  : "https://mobile.twitter.com/sana_natori",
+    "セプタ"    : "https://mobile.twitter.com/interceptor128"
 }
 
-
+# Twitter ID number 辞書
 tw_id_dic = {
     "七瀬すばる" : 1027562205347102720,
-    "しぐれうい" : 69496975
+    "しぐれうい" : 69496975,
+    "カグラナナ" : 4080861612,
+    "名取さな"  : 968546584337072128,
+    "セプタ"    : 134097420
 }
 
 #https://bitly.com で短縮したチャンネルURL
 #https://www.youtube.com/channel/***********
 bitly_yt_dic = {
     "七瀬すばる" : "https://bit.ly/2z9PzFG",
-    "しぐれうい" : "https://bit.ly/2Y8leQv"
+    "しぐれうい" : "https://bit.ly/2Y8leQv",
+    "カグラナナ" : "https://bit.ly/3h4vLEP",
+    "名取さな"  : "https://bit.ly/2APLVBe",
+    "セプタ"    : "https://bit.ly/30kALzg"
 }
 
 
