6b1dd537ee6e5b554ea8ec91d0039cdc37ce0de6
interceptor128 <polestar.interceptor.0128@gmail.com>
2020-06-09 20:38:11 +0900

    delete member by natori_sana

--- dics.py
+++ dics.py
@@ -1,28 +1,25 @@
 # botが見るVTuberのリスト
-members = ["七瀬すばる", "しぐれうい", "カグラナナ", "名取さな"]
+members = ["七瀬すばる", "しぐれうい", "カグラナナ"]
 
 # YouTube Ch の Channel ID　の辞書
 YT_ID_dic = {
     "七瀬すばる" : "UCji4kFR4n5TnEpqga0SHxlQ",
     "しぐれうい" : "UCt30jJgChL8qeT9VPadidSw",
-    "カグラナナ" : "UCbfv8uuUXt3RSJGEwxny5Rw",
-    "名取さな"  : "UCIdEIHpS0TdkqRkHL5OkLtA" 
+    "カグラナナ" : "UCbfv8uuUXt3RSJGEwxny5Rw"
 }
 
 # Twitter mobile版URLの辞書
 tw_url_dic = {
     "七瀬すばる" : "https://mobile.twitter.com/nanase_subaru_",
     "しぐれうい" : "https://mobile.twitter.com/ui_shig",
-    "カグラナナ" : "https://mobile.twitter.com/nana_kaguraaa",
-    "名取さな"  : "https://mobile.twitter.com/sana_natori"
-}
+    "カグラナナ" : "https://mobile.twitter.com/nana_kaguraaa"
+}  
 
 # Twitter ID number 辞書
 tw_id_dic = {
     "七瀬すばる" : 1027562205347102720,
     "しぐれうい" : 69496975,
-    "カグラナナ" : 4080861612,
-    "名取さな"  : 968546584337072128
+    "カグラナナ" : 4080861612
 }
 
 #https://bitly.com で短縮したチャンネルURL
@@ -30,8 +27,7 @@ tw_id_dic = {
 bitly_yt_dic = {
     "七瀬すばる" : "https://bit.ly/2z9PzFG",
     "しぐれうい" : "https://bit.ly/2Y8leQv",
-    "カグラナナ" : "https://bit.ly/3h4vLEP",
-    "名取さな"  : "https://bit.ly/2APLVBe"
+    "カグラナナ" : "https://bit.ly/3h4vLEP"
 }
 
 
