1a400977e5e921a0c7b4ed2d72e2e311742c143e
interceptor128 <polestar.interceptor.0128@gmail.com>
2020-07-21 23:02:45 +0900

    add 4 menbers

--- dics.py
+++ dics.py
@@ -1,12 +1,15 @@
 # botが見るVTuberのリスト
-members = ["七瀬すばる", "しぐれうい", "カグラナナ", "焼まゆる"]
+members = ["七瀬すばる", "しぐれうい", "カグラナナ", "焼まゆる", "エルゼ", "村瀬巴", "千莉", "佐久間ねむ"]
 
 # YouTube Ch の Channel ID　の辞書
 YT_ID_dic = {
     "七瀬すばる" : "UCji4kFR4n5TnEpqga0SHxlQ",
     "しぐれうい" : "UCt30jJgChL8qeT9VPadidSw",
     "カグラナナ" : "UCbfv8uuUXt3RSJGEwxny5Rw",
-    "焼まゆる"  : "UCg7aL_drCGzlPCrV6Hys_-w"
+    "焼まゆる" : "UCg7aL_drCGzlPCrV6Hys_-w",
+    "エルゼ" : "UCmjRV2ULqBFenjbsFIrerNg",
+    "千莉" : "UCrfI1f0FF-VpIBzposSB5Pg",
+    "佐久間ねむ" : "UCtjxl1Uf9-Xk2XdpPdMrccQ"
 }
 
 # Twitter mobile版URLの辞書
@@ -14,7 +17,11 @@ tw_url_dic = {
     "七瀬すばる" : "https://mobile.twitter.com/nanase_subaru_",
     "しぐれうい" : "https://mobile.twitter.com/ui_shig",
     "カグラナナ" : "https://mobile.twitter.com/nana_kaguraaa",
-    "焼まゆる"  : "https://mobile.twitter.com/yaki_mayu"
+    "焼まゆる" : "https://mobile.twitter.com/yaki_mayu",
+    "エルゼ" : "https://mobile.twitter.com/Else_ASMR",
+    "村瀬巴" : "https://mobile.twitter.com/Murase_Tomoe",
+    "千莉" : "https://mobile.twitter.com/V_Economist",
+    "佐久間ねむ" : "https://mobile.twitter.com/NNemuu_"
 }
 
 
@@ -22,7 +29,11 @@ tw_id_dic = {
     "七瀬すばる" : 1027562205347102720,
     "しぐれうい" : 69496975,
     "カグラナナ" : 4080861612,
-    "焼まゆる"  : 121674699
+    "焼まゆる" : 121674699,
+    "エルゼ" : 1058250264019263489,
+    "村瀬巴" : 981867535984295937,
+    "千莉" : 1073516137667653632,
+    "佐久間ねむ" : 1080744290978979841
 }
 
 #https://bitly.com で短縮したチャンネルURL
@@ -31,5 +42,8 @@ bitly_yt_dic = {
     "七瀬すばる" : "https://bit.ly/2z9PzFG",
     "しぐれうい" : "https://bit.ly/2Y8leQv",
     "カグラナナ" : "https://bit.ly/3h4vLEP",
-    "焼まゆる"  : "https://bit.ly/2XHVtYg"
+    "焼まゆる"  : "https://bit.ly/2XHVtYg",
+    "エルゼ" : "https://bit.ly/3jso9xh",
+    "千莉" : "https://bit.ly/3eLtPyz",
+    "佐久間ねむ" : "https://bit.ly/2ZMf4Hw"
 }
