6a1d835a690e2c1ade7f4c22bd71855cf59e157f
interceptor128 <polestar.interceptor.0128@gmail.com>
2021-04-16 20:17:47 +0900

    おめがシスターズ 追加

--- dics.py
+++ dics.py
@@ -1,46 +1,50 @@
 # botが見るVTuberのリスト
-members = ["七瀬すばる", "しぐれうい", "カグラナナ", "焼まゆる", "エルゼ", "千莉", "佐久間ねむ"]
+members = ["七瀬すばる", "しぐれうい", "カグラナナ", "焼まゆる", "エルゼ", "千莉", "佐久間ねむ", "おめがシスターズ"]
 
 # YouTube Ch の Channel ID　の辞書
 YT_ID_dic = {
-    "七瀬すばる" : "UCji4kFR4n5TnEpqga0SHxlQ",
-    "しぐれうい" : "UCt30jJgChL8qeT9VPadidSw",
-    "カグラナナ" : "UCbfv8uuUXt3RSJGEwxny5Rw",
-    "焼まゆる" : "UCg7aL_drCGzlPCrV6Hys_-w",
-    "エルゼ" : "UCmjRV2ULqBFenjbsFIrerNg",
-    "千莉" : "UCrfI1f0FF-VpIBzposSB5Pg",
-    "佐久間ねむ" : "UCtjxl1Uf9-Xk2XdpPdMrccQ"
+    "七瀬すばる": "UCji4kFR4n5TnEpqga0SHxlQ",
+    "しぐれうい": "UCt30jJgChL8qeT9VPadidSw",
+    "カグラナナ": "UCbfv8uuUXt3RSJGEwxny5Rw",
+    "焼まゆる": "UCg7aL_drCGzlPCrV6Hys_-w",
+    "エルゼ": "UCmjRV2ULqBFenjbsFIrerNg",
+    "千莉": "UCrfI1f0FF-VpIBzposSB5Pg",
+    "佐久間ねむ": "UCtjxl1Uf9-Xk2XdpPdMrccQ",
+    "おめがシスターズ": "UCNjTjd2-PMC8Oo_-dCEss7A"
 }
 
 # Twitter mobile版URLの辞書
 tw_url_dic = {
-    "七瀬すばる" : "https://mobile.twitter.com/nanase_subaru_",
-    "しぐれうい" : "https://mobile.twitter.com/ui_shig",
-    "カグラナナ" : "https://mobile.twitter.com/nana_kaguraaa",
-    "焼まゆる" : "https://mobile.twitter.com/yaki_mayu",
-    "エルゼ" : "https://mobile.twitter.com/Else_ASMR",
-    "千莉" : "https://mobile.twitter.com/V_Economist",
-    "佐久間ねむ" : "https://mobile.twitter.com/NNemuu_"
+    "七瀬すばる": "https://mobile.twitter.com/nanase_subaru_",
+    "しぐれうい": "https://mobile.twitter.com/ui_shig",
+    "カグラナナ": "https://mobile.twitter.com/nana_kaguraaa",
+    "焼まゆる": "https://mobile.twitter.com/yaki_mayu",
+    "エルゼ": "https://mobile.twitter.com/Else_ASMR",
+    "千莉": "https://mobile.twitter.com/V_Economist",
+    "佐久間ねむ": "https://mobile.twitter.com/NNemuu_",
+    "おめがシスターズ": "https://mobile.twitter.com/omesis_ray"
 }
 
 tw_id_dic = {
-    "七瀬すばる" : 1027562205347102720,
-    "しぐれうい" : 69496975,
-    "カグラナナ" : 4080861612,
-    "焼まゆる" : 121674699,
-    "エルゼ" : 1058250264019263489,
-    "千莉" : 1073516137667653632,
-    "佐久間ねむ" : 1080744290978979841
+    "七瀬すばる": 1027562205347102720,
+    "しぐれうい": 69496975,
+    "カグラナナ": 4080861612,
+    "焼まゆる": 121674699,
+    "エルゼ": 1058250264019263489,
+    "千莉": 1073516137667653632,
+    "佐久間ねむ": 1080744290978979841,
+    "おめがシスターズ": 968670513005346816
 }
 
-#https://bitly.com で短縮したチャンネルURL
-#https://www.youtube.com/channel/***********
+# https://bitly.com で短縮したチャンネルURL
+# https://www.youtube.com/channel/***********
 bitly_yt_dic = {
-    "七瀬すばる" : "https://bit.ly/2FHlxMI",
-    "しぐれうい" : "https://bit.ly/3krAd1a",
-    "カグラナナ" : "https://bit.ly/3cgJ7M4",
-    "焼まゆる"  : "https://bit.ly/3iMqdzn",
-    "エルゼ" : "https://bit.ly/3jso9xh",
-    "千莉" : "https://bit.ly/3eLtPyz",
-    "佐久間ねむ" : "https://bit.ly/2ZMf4Hw"
+    "七瀬すばる": "https://bit.ly/2FHlxMI",
+    "しぐれうい": "https://bit.ly/3krAd1a",
+    "カグラナナ": "https://bit.ly/3cgJ7M4",
+    "焼まゆる": "https://bit.ly/3iMqdzn",
+    "エルゼ": "https://bit.ly/3jso9xh",
+    "千莉": "https://bit.ly/3eLtPyz",
+    "佐久間ねむ": "https://bit.ly/2ZMf4Hw",
+    "おめがシスターズ": "https://bit.ly/3mRgevr"
 }
